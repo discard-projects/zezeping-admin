0b4a7251f161868b59ddbb279d20243f6cdf329d
mars <578595193@qq.com>
2018-03-07 12:53:12 +0800

    search store

--- src/views/Store/Index.vue
+++ src/views/Store/Index.vue
@@ -34,10 +34,8 @@ export default {
       searches: [
         {
           control: 'input',
-          key: 'q_email_eq',
-          placeholder: '邮箱'
-        }, {
-          key: 'q_roles_name_eq'
+          key: 'q_name_cont',
+          placeholder: '名称'
         }
       ]
     }
